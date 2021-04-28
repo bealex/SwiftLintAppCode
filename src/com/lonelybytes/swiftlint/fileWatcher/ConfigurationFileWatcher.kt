e35aeb881452121a28c7445c24e4e284e4578dab
Alex Babaev <alex@jdnevnik.com>
2021-04-28 12:02:59 +0300

    Fixed problem with not using configuration file right after it was added (file change needed previously).

---
+++ src/com/lonelybytes/swiftlint/fileWatcher/ConfigurationFileWatcher.kt
@@ -0,0 +1,16 @@
+package com.lonelybytes.swiftlint.fileWatcher
+
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.newvfs.BulkFileListener
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent
+
+class ConfigurationFileWatcher(val project: Project): BulkFileListener {
+    override fun after(events: MutableList<out VFileEvent>) {
+        super.after(events)
+
+        if (events.count { it.path.endsWith(".swiftlint.yml") } != 0) {
+            DaemonCodeAnalyzer.getInstance(project).restart()
+        }
+    }
+}
